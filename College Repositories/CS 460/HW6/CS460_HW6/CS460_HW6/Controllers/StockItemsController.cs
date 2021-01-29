edb716115821c1e5c5fc80be3e04994c55978db0
Matthew Hibner <matthewhibner3@gmail.com>
2021-01-28 18:18:21 -0800

    Add files via upload
    add hw6 to 460 folder

---
+++ College Repositories/CS 460/HW6/CS460_HW6/CS460_HW6/Controllers/StockItemsController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using CS460_HW6.DAL;
+using CS460_HW6.Models;
+using CS460_HW6.Models.ViewModels;
+
+namespace CS460_HW6.Controllers
+{
+    public class StockItemsController : Controller
+    {
+        private WWIContext db = new WWIContext();
+
+        // GET: StockItems
+        public ActionResult Index()
+        {
+            var stockItems = db.StockItems.Where(m => m.StockItemID == 0);
+            //ViewBag.Search = "USB";
+            //var stockItems = db.StockItems;
+            return View(stockItems.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string searchInput)
+        {
+            var stockItems = db.StockItems.Where(m => m.StockItemName.Contains(searchInput));
+            //ViewBag.Search = search;
+            //var stockItems = db.StockItems;
+            return View(stockItems.ToList());
+        }
+
+
+        // GET: StockItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockItem stockItem = db.StockItems.Find(id);
+
+    
+            Person contact = db.People.Find(db.StockItems.Find(id).Supplier.PrimaryContactPersonID);
+
+            
+
+            decimal totalOrders = db.OrderLines.Where(m => m.StockItem.StockItemID == id).Select(t => t.Quantity).Count();
+            decimal grossSales = db.InvoiceLines.Where(m => m.StockItem.StockItemID == id).Sum(t => t.ExtendedPrice);
+            decimal grossProfit = db.InvoiceLines.Where(m => m.StockItem.StockItemID == id).Sum(t => t.LineProfit);
+
+
+            var orderList = db.InvoiceLines.Where(t => t.StockItem.StockItemID == id).GroupBy(t => t.Invoice.Customer.CustomerName).Select(s => new { Buyer = s.Key, Amount = s.Sum(v => v.Quantity) }).OrderByDescending(x => x.Amount).Take(10).ToArray();
+            //var orderList = db.InvoiceLines.Where(t => t.StockItem.StockItemID == 129).GroupBy(t => t.Invoice.Customer.CustomerName).Select(s => new { Buyer = s.Key, Amount = s.Sum(v => v.Quantity) }).OrderByDescending(x => x.Amount).Take(10);
+            int length = orderList.Length;
+            string[] buyerNames = new string[length];
+            int[] buyerAmounts = new int[length];
+
+            for(int i = 0; i < length; i++)
+            {
+                buyerNames[i] = orderList[i].Buyer;
+                buyerAmounts[i] = orderList[i].Amount;
+            }
+
+            ProductDetails details = new ProductDetails(stockItem, contact, totalOrders, grossSales, grossProfit, buyerNames, buyerAmounts);
+
+            if (stockItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(details);
+        }
+  
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ College Repositories/CS 460/HW6/CS460_HW6/CS460_HW6/Models/ViewModels/ProductDetails.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Script.Serialization;
+
+namespace CS460_HW6.Models.ViewModels
+{
+    public class ProductDetails
+    {
+      
+
+        public ProductDetails(StockItem product,Person contact, decimal totalOrders, decimal grossSales, decimal grossProfit, string[] buyerName, int[] buyerAmount)
+         {
+            StockItemName = product.StockItemName;
+            Size = product.Size;
+            RecommendedRetailPrice = product.RecommendedRetailPrice;
+            TypicalWeightPerUnit = product.TypicalWeightPerUnit;
+            LeadTimeDays = product.LeadTimeDays;
+
+            
+            ValidFrom = product.ValidFrom.Date.ToString();
+
+
+            //https://stackoverflow.com/questions/12676746/parse-json-string-in-c-sharp
+            dynamic customFields = Newtonsoft.Json.JsonConvert.DeserializeObject(product.CustomFields);
+            string origin = customFields.CountryOfManufacture;
+
+            CustomFields = origin;
+
+            //https://stackoverflow.com/questions/7411438/remove-characters-from-c-sharp-string
+              string tags = String.Join("", product.Tags.Split('[', ']', '{', '}','"'));
+            Tags = tags;
+
+            Photo = product.Photo;
+            
+            SupplierName = product.Supplier.SupplierName;
+            SupplierPhoneNumber = product.Supplier.PhoneNumber;
+            SupplierFaxNumber = product.Supplier.FaxNumber;
+            SupplierWebsiteURL = product.Supplier.WebsiteURL;
+            SupplierContactFullName = contact.FullName;
+
+            TotalOrders = Decimal.ToInt32(totalOrders);
+            GrossSales = grossSales;
+            GrossProfit = grossProfit;
+
+            BuyerNames = buyerName;
+            BuyerAmount = buyerAmount;
+
+
+
+
+
+         }
+
+
+        public string StockItemName { get; set; }
+
+        [StringLength(20)]
+        public string Size { get; set; }
+
+        public decimal? RecommendedRetailPrice { get; set; }
+
+        public decimal TypicalWeightPerUnit { get; set; }
+
+        public int LeadTimeDays { get; set; }
+
+        
+        public String ValidFrom { get; set; }
+
+        public string CustomFields { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public string Tags { get; set; }
+
+        public byte[] Photo { get; set; }
+
+
+        //Supplier Section
+
+        [StringLength(100)]
+        public string SupplierName { get; set; }
+
+        [StringLength(20)]
+        public string SupplierPhoneNumber { get; set; }
+
+        [StringLength(20)]
+        public string SupplierFaxNumber { get; set; }
+
+        [StringLength(256)]
+        public string SupplierWebsiteURL { get; set; }
+
+        [StringLength(50)]
+        public string SupplierContactFullName { get; set; }
+
+        public int TotalOrders { get; set; }
+
+        public decimal GrossSales { get; set; }
+
+        public decimal GrossProfit { get; set; }
+
+        public string[] BuyerNames { get; set; }
+
+        public int[] BuyerAmount { get; set; }
+    }
+}
